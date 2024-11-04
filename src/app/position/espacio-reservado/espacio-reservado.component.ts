1fad0fe58bbad169e685cfeafdd3a99c0cac98a9
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-03 19:32:20 -0600

    inicializando el proyecto de css avanzado con angular

---
+++ src/app/position/espacio-reservado/espacio-reservado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-espacio-reservado',
+  standalone: true,
+  imports: [],
+  templateUrl: './espacio-reservado.component.html',
+  styleUrl: './espacio-reservado.component.css'
+})
+export class EspacioReservadoComponent {
+
+}
