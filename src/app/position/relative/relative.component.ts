ba7e44fa2f908c5a1cb6af466472969801c7d3df
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-09 08:54:37 -0600

    Tema: position relative, y con esta nueva propiedad modifico a mi menú

---
+++ src/app/position/relative/relative.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relative',
+  standalone: true,
+  imports: [],
+  templateUrl: './relative.component.html',
+  styleUrl: './relative.component.css'
+})
+export class RelativeComponent {
+
+}
--- src/app/position/routes.ts
+++ src/app/position/routes.ts
@@ -2,9 +2,11 @@ import { Route } from "@angular/router";
 import { FlujoComponent } from "./flujo/flujo.component";
 import { EspacioReservadoComponent } from "./espacio-reservado/espacio-reservado.component";
 import { OffsetComponent } from "./offset/offset.component";
+import { RelativeComponent } from "./relative/relative.component";
 
 export default[
     {path:'flujo', component: FlujoComponent},
     {path:'espacio-reservado',component:EspacioReservadoComponent},
     {path:'offset',component:OffsetComponent},
+    {path:'relative',component: RelativeComponent},
 ] as Route[];
