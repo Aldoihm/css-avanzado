06a80f94b5e730fe349aea74c54468ef08897954
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-15 08:45:27 -0600

    Explicación de posicionamiento absoluto, aquí es importante vigilar al posicionamiento padre

---
+++ src/app/position/absolute/absolute.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-absolute',
+  standalone: true,
+  imports: [],
+  templateUrl: './absolute.component.html',
+  styleUrl: './absolute.component.css'
+})
+export class AbsoluteComponent {
+
+}
--- src/app/position/routes.ts
+++ src/app/position/routes.ts
@@ -3,10 +3,12 @@ import { FlujoComponent } from "./flujo/flujo.component";
 import { EspacioReservadoComponent } from "./espacio-reservado/espacio-reservado.component";
 import { OffsetComponent } from "./offset/offset.component";
 import { RelativeComponent } from "./relative/relative.component";
+import { AbsoluteComponent } from "./absolute/absolute.component";
 
-export default[
-    {path:'flujo', component: FlujoComponent},
-    {path:'espacio-reservado',component:EspacioReservadoComponent},
-    {path:'offset',component:OffsetComponent},
-    {path:'relative',component: RelativeComponent},
+export default [
+  { path: 'flujo', component: FlujoComponent },
+  { path: 'espacio-reservado', component: EspacioReservadoComponent },
+  { path: 'offset', component: OffsetComponent },
+  { path: 'relative', component: RelativeComponent },
+  { path: 'absolute', component: AbsoluteComponent }
 ] as Route[];
