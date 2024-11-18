2420e4644a52d58aa4441b806697c915164fa9b0
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-18 09:45:23 -0600

    Herramienta Tooltip, me gustó mucho para implementarlo en mis proyectos

--- src/app/position/routes.ts
+++ src/app/position/routes.ts
@@ -4,11 +4,13 @@ import { EspacioReservadoComponent } from "./espacio-reservado/espacio-reservado
 import { OffsetComponent } from "./offset/offset.component";
 import { RelativeComponent } from "./relative/relative.component";
 import { AbsoluteComponent } from "./absolute/absolute.component";
+import { TooltipComponent } from "./tooltip/tooltip.component";
 
 export default [
   { path: 'flujo', component: FlujoComponent },
   { path: 'espacio-reservado', component: EspacioReservadoComponent },
   { path: 'offset', component: OffsetComponent },
   { path: 'relative', component: RelativeComponent },
-  { path: 'absolute', component: AbsoluteComponent }
+  { path: 'absolute', component: AbsoluteComponent },
+  { path: 'tooltip', component: TooltipComponent },
 ] as Route[];
