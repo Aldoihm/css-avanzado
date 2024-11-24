e8ba09f79b59f6868f48df31056d905e31ee962a
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-24 11:04:32 -0600

    Segunda parte de fixed y tema completo de sticky, ambos interesantes y de más análisis

--- src/app/position2/fixed/fixed.component.ts
+++ src/app/position2/fixed/fixed.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fixed',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './fixed.component.css'
 })
 export class FixedComponent {
+  constructor(private router: Router) {}
 
+  scrollToSection(sectionId: string) {
+    this.router.navigate([],{ fragment: sectionId}).then(() => {
+      const element = document.getElementById(sectionId);
+      if(element){
+        element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'start'});
+      }
+    });
+  }
 }
