32d9a3a9b17f91a7b93340df38a426b1bc72bd2a
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-25 08:15:11 -0600

    Apilamiento o stacking (que son las capas), introducción a z-index

--- src/app/position2/routes.ts
+++ src/app/position2/routes.ts
@@ -1,6 +1,8 @@
 import { Route } from "@angular/router";
 import { FixedComponent } from "./fixed/fixed.component";
+import { StackingComponent } from "./stacking/stacking.component";
 
 export default [
     { path: 'fixed', component: FixedComponent },
+    { path: 'stacking', component: StackingComponent },
 ] as Route[];
---
+++ src/app/position2/stacking/stacking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stacking',
+  standalone: true,
+  imports: [],
+  templateUrl: './stacking.component.html',
+  styleUrl: './stacking.component.css'
+})
+export class StackingComponent {
+
+}
