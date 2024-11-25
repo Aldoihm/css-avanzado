cdf4ca050093eaf3da83f72e2797a63b9b7cf21f
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-25 08:52:25 -0600

    z-index para controlar el apilamiento y reglas entre elementos padres y elementos hijos con z-index

--- src/app/position2/routes.ts
+++ src/app/position2/routes.ts
@@ -1,8 +1,10 @@
 import { Route } from "@angular/router";
 import { FixedComponent } from "./fixed/fixed.component";
 import { StackingComponent } from "./stacking/stacking.component";
+import { ZIndexComponent } from "./z-index/z-index.component";
 
 export default [
     { path: 'fixed', component: FixedComponent },
     { path: 'stacking', component: StackingComponent },
+    { path: 'z-index', component: ZIndexComponent },
 ] as Route[];
---
+++ src/app/position2/z-index/z-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-z-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './z-index.component.html',
+  styleUrl: './z-index.component.css'
+})
+export class ZIndexComponent {
+
+}
