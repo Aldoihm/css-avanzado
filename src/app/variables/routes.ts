524b4d1a8ba44dfe08478c5e572c6455aaa78ad2
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-11-30 09:52:28 -0600

    Anotaciones de reglas Scope

--- src/app/variables/routes.ts
+++ src/app/variables/routes.ts
@@ -1,6 +1,8 @@
 import { Route } from "@angular/router";
 import { VariablesCssComponent } from "./variables-css/variables-css.component";
+import { ScopeComponent } from "./scope/scope.component";
 
 export default [
   { path: 'variables-css', component: VariablesCssComponent },
+  { path: 'scope', component: ScopeComponent},
 ] as Route[];
---
+++ src/app/variables/scope/scope.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scope',
+  standalone: true,
+  imports: [],
+  templateUrl: './scope.component.html',
+  styleUrl: './scope.component.css'
+})
+export class ScopeComponent {
+
+}
