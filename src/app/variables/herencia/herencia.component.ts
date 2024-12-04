a41958a1104dfae7e0b32fedcaadc4c21eb158a8
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-12-03 23:20:37 -0600

    Herencia con variables css, no escribí nada de cascada porque me parece obvio el tema

---
+++ src/app/variables/herencia/herencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-herencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './herencia.component.html',
+  styleUrl: './herencia.component.css'
+})
+export class HerenciaComponent {
+
+}
--- src/app/variables/routes.ts
+++ src/app/variables/routes.ts
@@ -1,8 +1,10 @@
 import { Route } from "@angular/router";
 import { VariablesCssComponent } from "./variables-css/variables-css.component";
 import { ScopeComponent } from "./scope/scope.component";
+import { HerenciaComponent } from "./herencia/herencia.component";
 
 export default [
   { path: 'variables-css', component: VariablesCssComponent },
   { path: 'scope', component: ScopeComponent},
+  { path: 'herencia', component: HerenciaComponent},
 ] as Route[];
