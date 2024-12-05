b7d553a3549a9cf223ccddffe24fa2550b1a2129
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-12-05 11:53:54 -0600

    Uso de variables en responsive web desing RWB

--- src/app/variables/routes.ts
+++ src/app/variables/routes.ts
@@ -2,9 +2,11 @@ import { Route } from "@angular/router";
 import { VariablesCssComponent } from "./variables-css/variables-css.component";
 import { ScopeComponent } from "./scope/scope.component";
 import { HerenciaComponent } from "./herencia/herencia.component";
+import { RwdComponent } from "./rwd/rwd.component";
 
 export default [
   { path: 'variables-css', component: VariablesCssComponent },
   { path: 'scope', component: ScopeComponent},
   { path: 'herencia', component: HerenciaComponent},
+  { path: 'rwd', component: RwdComponent},
 ] as Route[];
---
+++ src/app/variables/rwd/rwd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rwd',
+  standalone: true,
+  imports: [],
+  templateUrl: './rwd.component.html',
+  styleUrl: './rwd.component.css'
+})
+export class RwdComponent {
+
+}
